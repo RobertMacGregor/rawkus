1cc0265bda0d0a17a81a6b9a2ef85ca627319903
Robert MacGregor <rob@rmacgregor.net>
2021-02-25 13:17:20 -0500

    Add CTranspilerVisitor.java to extend CBaseVisitor so that parse-tree visits can be intercepted and added to the Tinkerpop graph db.
    Added TinkerGraph jar file/library to provide TinkerGraph type. Jar file source from Tinkerpop server ext folder.

--- src/CTranspilerVisitor.java
+++ src/CTranspilerVisitor.java
@@ -1,2 +1,54 @@
-public class CTranspilerVisitor extends CBaseVisitor{
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+
+public class CTranspilerVisitor extends CBaseVisitor<CymVertex>{
+    GraphTraversalSource g;
+
+    CTranspilerVisitor(GraphTraversalSource newGTS) {
+        g = newGTS;
+    }
+
+    protected CymVertex defaultResult() {
+        return new CymVertex();
+    }
+
+    protected CymVertex aggregateResult( CymVertex aggregate, CymVertex nextResult) {
+        //System.out.println("aggregateResult "+nextResult);
+        aggregate.addChild(nextResult);
+        //return nextResult;
+        return aggregate;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        TinkerGraph graph = TinkerGraph.open();
+        org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource  g = traversal().withEmbedded(graph);
+
+
+        InputStream is = System.in;
+        is = new FileInputStream(args[0]);
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        CLexer lexer = new CLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CParser parser = new CParser(tokens);
+        ParseTree tree = parser.primaryExpression();
+
+        CTranspilerVisitor myTestVisitor = new CTranspilerVisitor(g);
+        myTestVisitor.visit(tree);
+
+        g.io("transpiler.graph.kryo").write().iterate();
+        graph.close();
+
+
+
+    }
 }
